b1542b7e354d283d621942ecca9f8f60f8f978c1
juanguillermolopez <juan.guillermo.lopez.garcia@gmail.com>
2023-07-18 02:51:10 -0400

    subiendo las notis de nuevo

--- bd/diseño_web_noticias.sql
+++ bd/diseño_web_noticias.sql
@@ -37,7 +37,7 @@ CREATE TABLE `noticias` (
 
 LOCK TABLES `noticias` WRITE;
 /*!40000 ALTER TABLE `noticias` DISABLE KEYS */;
-INSERT INTO `noticias` VALUES (1,'Noticia 1','imagen1.jpg','Contenido de la noticia 1'),(2,'Noticia 2','imagen2.jpg','Contenido de la noticia 2'),(3,'Noticia 3','imagen3.jpg','Contenido de la noticia 3'),(4,'Noticia 4','imagen4.jpg','Contenido de la noticia 4'),(5,'Noticia 5','imagen5.jpg','Contenido de la noticia 5'),(6,'voyager','Diseño sin título.png','con su tripulacion'),(7,'xxxxxxxxxxxxxxxxx','IMG-20221223-WA0000.jpeg','xxxxxxxxxxxxxxxxx');
+INSERT INTO `noticias` VALUES (1,'Idolo','220px-Richard_Stallman_2005_(chrys).jpg','El inmortal Richard Stallman.'),(2,'Comida saludable.','Captura de pantalla 2023-03-18 215834.png','Hasta la boleta te sirve.'),(3,'Suchi con reparto.','Captura de pantalla 2023-05-24 195714.png','60 piezas a 2 lucas.\r\n18 lucas el reparto.'),(4,'Se arreinda casa','6919078961.jpg','casa en Amanecer. $150.000.-'),(5,'Dicaprio','356158029_1173178733529049_6744484363834764962_n.jpg','Un poco de humor sin arrepentirse'),(6,'voyager','Diseño sin título.png','con su tripulacion'),(7,'YO','IMG-20221223-WA0000.jpeg','Hace unos meses en el trabajo');
 /*!40000 ALTER TABLE `noticias` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-18  1:57:15
+-- Dump completed on 2023-07-18  2:45:06
