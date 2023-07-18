83c7dc06170b207bfcfce721306b6a73f6bae32f
juanguillermolopez <juan.guillermo.lopez.garcia@gmail.com>
2023-07-18 04:09:31 -0400

    ajustando los menus, otra vez, ufff

--- news/news.php
+++ news/news.php
@@ -3,8 +3,10 @@
 <head>
   <title>Noticias</title>
 	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
   <style>
     .news-container {
       margin-top: 20px;
@@ -33,7 +35,7 @@
 			  <ul class="navbar-nav">
 
 				<li class="nav-item active  ">
-				  <a class="nav-link text-white" href="startrek.php">Inicio</a>
+				  <a class="nav-link text-white" href="../startrek.php">Inicio</a>
 				</li>
 
 				<li class="nav-item dropdown ">
@@ -85,5 +87,17 @@
 
     <a href="add_news.php" class="btn btn-success mt-4">Agregar noticia</a>
   </div>
+	
+	
+	
+	
+	
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
 </body>
 </html>
